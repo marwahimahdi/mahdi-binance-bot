ff0431c97ac8b15105225489f6137b5b918203ed
marwahimahdi <mahdi.marwahi11@gmail.com>
2025-10-19 09:35:30 +0300

    Update bot_v5_final.py

--- bot_v5_final.py
+++ bot_v5_final.py
@@ -94,9 +94,9 @@ def send_tg(text):
 _time_offset_ms=0
 
 def main():
-print("[DEBUG] SYMBOLS_CSV =", repr(SYMBOLS_CSV))
-send_tg(f"[DEBUG] SYMBOLS_CSV={SYMBOLS_CSV}")
-sync_server_time()
+    print("[DEBUG] SYMBOLS_CSV =", repr(SYMBOLS_CSV))
+    send_tg(f"[DEBUG] SYMBOLS_CSV={SYMBOLS_CSV}")
+    sync_server_time()
     global _time_offset_ms
     try:
         r=session.get(SERVER_TIME_EP, timeout=10); r.raise_for_status()
